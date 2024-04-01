07a0b5541a02161817da34619efc83bd136ced26
Aaron-Huntley <aaronhuntley220101@gmail.com>
2024-04-01 16:51:47 -0400

    Added percentage comparison

--- cluster-analysis.py
+++ cluster-analysis.py
@@ -8,18 +8,28 @@ This is a temporary script file.
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import dendrogram, linkage
-from sklearn.datasets import make_blobs
 from sklearn.cluster import AgglomerativeClustering
 import pandas as pd
 
 
-def import_interoception(file_path):
-    data = pd.read_csv(file_path)
+def import_grounding(data,grounding):
     
-    #A is interoception
-    A = [data.Word.values,data.Interoceptive_Lancaster.values]
+    '''NEED TO NORMALISE THIS'''
     
-    # #remove unrated words
+    if grounding == "Interoceptive":
+        #0-5
+        A = [data.Word.values,data.Interoceptive_Lancaster.values]
+    elif grounding == "Socialness":
+        #1-7
+        A = [data.Word.values,data.Socialness.values]
+    elif grounding == "Valence_Warriner":
+        #0-9
+        A = [data.Word.values,data.Valence_Warriner.values]
+    elif grounding == "Arousal_Warriner":
+        #0-9
+        A = [data.Word.values,data.Arousal_Warriner.values]
+        
+    #remove unrated words
     A[1] = A[1].astype(float)
     float_mask = ~np.isnan(A[1])
     A[0] = A[0][float_mask]
@@ -34,7 +44,6 @@ def distance_matrix(raw):
     # Convert interoceptive ratings to a NumPy array
     interoceptive_ratings_np = np.array(interoceptive_ratings)
     
-
     # Calculate absolute differences between all pairs of interoceptive ratings
     M = np.abs(interoceptive_ratings_np[:, np.newaxis] + interoceptive_ratings_np)
     M[np.diag_indices(n)] = 0
@@ -42,16 +51,20 @@ def distance_matrix(raw):
     return M
 
     
-def higherarchecal_clustering(M):
+def higherarchecal_clustering(M,D):
     # Creating a dendrogram
     # You can change the linkage method as per your requirement
     plt.figure(figsize=(10, 7))
     linked = linkage(M, 'single')
-    dendrogram(linked, orientation='top', distance_sort='descending', show_leaf_counts=True)
-    plt.title('Dendrogram')
-    plt.xlabel('Word Index')
-    plt.ylabel('Distance')
-    plt.show()
+    
+    if D == True:
+        dendrogram(linked, orientation='top', distance_sort='descending', show_leaf_counts=True)
+        plt.title('Dendrogram')
+        plt.xlabel('Word Index')
+        plt.ylabel('Distance')
+        plt.show()
+    
+    return linked
     
     # Performing agglomerative clustering
     #cluster = AgglomerativeClustering(n_clusters=4, affinity='precomputed', linkage='ward')
@@ -70,15 +83,51 @@ def filter_distance(raw,d):
     
     return rawf
 
-def words_back(cluster,linked):
-    W = []
+
+def jaccard_similarity(set1, set2):
+    intersection = len(set1.intersection(set2))
+    union = len(set1.union(set2))
+    similarity = intersection / union if union != 0 else 0  # Handle division by zero
+    return similarity * 100
+
+def percent_compare(G1,G1N,G2,G2N,D):
+
+    combined_G1 = list(zip(G1[0],G1[1]))
+    ordered_pairs_G1 = sorted(combined_G1, key=lambda x: x[1])
+    ordered_strings_G1, ordered_floats_G1 = zip(*ordered_pairs_G1)
     
+    combined_G2 = list(zip(G2[0],G2[1]))
+    ordered_pairs_G2 = sorted(combined_G2, key=lambda x: x[1])
+    ordered_strings_G2, ordered_floats_G2 = zip(*ordered_pairs_G2)
     
+    #Assuming the normalised data is 0-8  
+    x = np.linspace(0, 8, 1000)
+    y=[]
+    for i in x:
+        G1Str = [string for string, value in ordered_pairs_G1 if value < i]
+        G2Str = [string for string, value in ordered_pairs_G2 if value < i]
+        y.append(jaccard_similarity(set(G1Str),set(G2Str)))
     
-    return W
+
+    if D == True:
+        plt.plot(x,y)
+        plt.title('Comparison of '+str(G1N)+" and "+str(G2N))
+        plt.xlabel('Words of distance < x')
+        plt.ylabel('Percent Similarity')
+        plt.show()
+        
+    
+    return y
 
 
-intero = import_interoception("Dataset.csv")
+
+
+''''TEST RUNNING'''
+
+data = pd.read_csv("Dataset.csv")
+
+'''Test dendogram
+intero = import_grounding(data,"Interoceptive")
 
 #Truncated data
 interoT = [[],[]]
@@ -89,8 +138,27 @@ interoT[1] = intero[1][:100]
 interoD = filter_distance(intero,1)
 
 M = distance_matrix(interoD)
-higherarchecal_clustering(M)
+interolinked  = higherarchecal_clustering(M,False)
+'''
 
+'''Test percentage graphs'''
+
+valence = import_grounding(data,"Valence_Warriner")
+arousal = import_grounding(data,"Arousal_Warriner")
+social = import_grounding(data,"Socialness")
+
+valenceT = [[],[]]
+valenceT[0] = valence[0][:10]
+valenceT[1] = valence[1][:10]
+
+arousalT = [[],[]]
+arousalT[0] = arousal[0][:10]
+arousalT[1] = arousal[1][:10]
+
+Val_Aro = percent_compare(valence,"Valence",arousal,"Arousal",True)
+Val_Soc = percent_compare(valence,"Valence",social,"Social",True)
+Aro_Soc = percent_compare(arousal,"Arousal",social,"Social",True)
+Aro_Aro = percent_compare(arousal,"Arousal",arousal,"Arousal",True)
 
 
     
